d38d3a984101f1485c6050ed4231529db2ce3f78
weslley-lima24 <weslley.lima24@etec.sp.gov.br>
2022-08-21 21:57:33 -0300

    Agora com snackbar e com verificar login

--- app/src/main/java/com/exemplolivro/dialogs/MainActivity.java
+++ app/src/main/java/com/exemplolivro/dialogs/MainActivity.java
@@ -4,26 +4,34 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity
 {
 
+    EditText userEmail, userSenha;
     Button btn_alert;
     AlertDialog alertDialog;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        userEmail = findViewById(R.id.user_email);
+        userSenha = findViewById(R.id.user_senha);
+        View view = findViewById(R.id.activity_main);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Deseja mesmo apertar esse botão?"); //mensagem
+        builder.setTitle("Confirmar");
+        builder.setMessage("Deseja mesmo ver esse texto?"); //mensagem
 
         //se sim
         builder.setPositiveButton("Sim", new DialogInterface.OnClickListener()
@@ -31,8 +39,21 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(DialogInterface dialogInterface, int i)
             {
-                Toast toast = Toast.makeText(getApplicationContext(), "Você apertou sim.", Toast.LENGTH_LONG);
-                toast.show();
+               String email = userEmail.getText().toString();
+               String senha = userSenha.getText().toString();
+
+               if(email.isEmpty() || senha.isEmpty())
+               {
+                   Snackbar snackbar = Snackbar.make(view, "Email ou senha vazios!", Snackbar.LENGTH_LONG);
+                   snackbar.setBackgroundTint(Color.rgb(255, 173, 0));
+                   snackbar.show();
+               }
+               else
+               {
+                   Toast toast = Toast.makeText(getApplicationContext(), "Login feito com sucesso.", Toast.LENGTH_LONG);
+                   toast.show();
+               }
+
             }
         });
 
@@ -58,6 +79,9 @@ public class MainActivity extends AppCompatActivity
                 alertDialog.show();
             }
         });
+
+
+
 
     }
 }
